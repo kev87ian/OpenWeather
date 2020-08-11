7e08b6add9650904ae4b09b2725244ccb999e709
kev87ian <kelvinian87@gmail.com>
2020-08-12 02:16:53 +0300

    final changes

--- app/src/main/java/com/kev/openweather/MainActivity.java
+++ app/src/main/java/com/kev/openweather/MainActivity.java
@@ -1,14 +1,84 @@
 package com.kev.openweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "error";
+    public static String BaseUrl = "https://api.openweathermap.org/";
+    public static String AppId = "2e65127e909e178d0af311a81f39948c";
+    public static String lat = "-1.196036";
+    public static String lon = "36.925285";
+    private TextView weatherData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weatherData = findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                getCurrentData();
+                Toast.makeText(getApplicationContext(), "Fetching data", Toast.LENGTH_LONG).show();
+            }
+        });
+}
+    void getCurrentData(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        WeatherService service = retrofit.create(WeatherService.class);
+        Call<WeatherResponse> call = service.getCurrentWeatherData(lat, lon, AppId);
+        call.enqueue(new Callback<WeatherResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<WeatherResponse> call, @NonNull Response<WeatherResponse> response) {
+                if (response.code() == 200) {
+                    Toast.makeText(getApplicationContext(), "Works as should", Toast.LENGTH_LONG).show();
+                    WeatherResponse weatherResponse = response.body();
+                    assert weatherResponse != null;
+
+                    String stringBuilder = "Country: " +
+                            weatherResponse.sys.country +
+                            "\n" +
+                            "Temperature: " +
+                            weatherResponse.main.temp +
+                            "\n" +
+                            "Temperature(Min): " +
+                            weatherResponse.main.temp_min +
+                            "\n" +
+                            "Temperature(Max): " +
+                            weatherResponse.main.temp_max +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main.pressure;
+
+                    weatherData.setText(stringBuilder);
+                }
+            }
+            @Override
+
+            public void onFailure(@NonNull Call<WeatherResponse> call, @NonNull Throwable t) {
+                weatherData.setText(t.getMessage());
+            Toast.makeText(getApplicationContext(), "Tough luck. I am facing technical difficulties. :(", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
